8cddf0d19d556e565fdb8014b0ea351da8625b82
Aditya <adityasingh62065@gmail.com>
2025-02-06 16:51:52 +0530

    first commit

---
+++ CricInfoBackend/src/main/java/com/aditya/CricInfoBackend/CricInfoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.CricInfoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricInfoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricInfoBackendApplication.class, args);
+	}
+
+}
